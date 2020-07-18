c72e70c077e5f7630b67563066aa16703ac12314
Joan Karadimov <joan.karadimov@gmail.com>
2020-07-18 17:58:14 +0300

    Add another test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include "seh.h"
 #include <stdio.h>
 
-int main() {
+void main1() {
   bool handler_executed = false;
   __try {
     int zero = atoi("0");
@@ -12,7 +12,23 @@ int main() {
 	   EXCEPTION_CONTINUE_SEARCH) {
     handler_executed = true;
   }
-  __except_end
 
-  printf("%d\n", handler_executed);
+  printf(handler_executed ? "OK\n" : "NOT OK\n");
+}
+
+int main() {
+  main1();
+
+  bool handler_executed = false;
+  __try {
+    int two = atoi("2");
+    int result = 1 / two;
+  }
+  __except(GetExceptionCode() == STATUS_INTEGER_DIVIDE_BY_ZERO ?
+	   EXCEPTION_EXECUTE_HANDLER :
+	   EXCEPTION_CONTINUE_SEARCH) {
+    handler_executed = true;
+  }
+
+  printf(handler_executed ? "NOT OK\n" : "OK\n");
 }
